79103310af989a4d20d0a1822fd7c106b399c91b
anjaliruth <ruthanjali0211@gmail.com>
2023-08-17 21:19:14 +0100

    login logic works, but just shows Welcome, need to render FizzBuzz next

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,22 @@
 
 import './App.css';
+import {useState} from 'react'
 import Authentication from './components/Auth/Authentication';
 import FizzBuzz from './components/FizzBuzz/FizzBuzz';
 
+
 function App() {
+  const [user, setUser] = useState(null)
+
+  function validUser(details) {
+    setUser(details)
+  }
+  
   return (
     <div className="FizzBuzzList">
          <h1 className='title'>FizzBuzz List for BlckBx</h1>
       {/* <FizzBuzz/> */}
-      <Authentication/>
+      <Authentication validUser={validUser}/>
     </div>
   );
 }
--- myapp/src/components/Auth/Authentication.js
+++ myapp/src/components/Auth/Authentication.js
@@ -1,24 +1,40 @@
-import { useState } from 'react';
+import { useState } from "react";
 
+// Move the dummyDataUsers outside of the component function
+const dummyDataUsers = [{ email: "123@test.com", password: "password1" }];
 
-export default function Authentication() {
-
+export default function Authentication({ validUser }) {
   // create states for username and password
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  function handleLogin() {
+    const approvedUser = dummyDataUsers.find(
+      (user) => user.email === email && user.password === password
+    );
 
+    if (approvedUser) {
+      setIsAuthenticated(true);
+      validUser(approvedUser);
+    } else {
+      alert("Either your username or password is incorrect. Please try again.");
+    }
+  }
 
   return (
     <div>
-{/* use a ternary operator to check if the correct credentials has been added */}
-{/* If not, show login input fields, if yes, say Welcome!*/}
-  
+      {/* use a ternary operator to check if the correct credentials have been added */}
+      {/* If not, show login input fields; if yes, say Welcome! */}
+      {isAuthenticated ? (
+        <p>Welcome!</p>
+      ) : (
         <div>
           <input
-            value={username}
-            placeholder="Enter username"
-            onChange={(e) => setUsername(e.target.value)}
+            type="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
@@ -26,9 +42,9 @@ export default function Authentication() {
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button>Login</button>
+          <button onClick={handleLogin}>Login</button>
         </div>
-
+      )}
     </div>
   );
 }
