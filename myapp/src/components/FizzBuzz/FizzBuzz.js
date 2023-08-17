34c1d52faebfdf1e0a0bd8519dbef6a717283d0d
anjaliruth <ruthanjali0211@gmail.com>
2023-08-17 12:57:20 +0100

    can fetch data from the backend to console

--- myapp/src/components/FizzBuzz/FizzBuzz.js
+++ myapp/src/components/FizzBuzz/FizzBuzz.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 export default function FizzBuzz() {
+        //create state to store the incoming data from API, and the default state is an empty array
+        const [fizzBuzzData, setFizzBuzzData] = useState([]);
+
+          //use useEffect hook to ensure that data is fetched from the backend at a desired time
+  // this 'time' mentioned above will be after the component mounts, and will only render after the component is mounted. (Done so by a second argument of [] in this useEffect hook. )
+    useEffect(() => {
+        async function fetchData() {
+    
+            const response = await fetch("http://127.0.0.1:5000/fizzbuzz");
+            const data = await response.json();
+            // Store incoming data from the backend in a state
+            setFizzBuzzData(data);
+            console.log(data);
+         
+        }
+      
+        fetchData();
+      }, []);
+    
+  
   return (
-    <div>FizzBuzz</div>
+    <div>
+
+    </div>
   )
 }
