036ea252d17a85ceea6301e6f62e24da1892342b
anjaliruth <ruthanjali0211@gmail.com>
2023-09-10 11:45:21 +0100

    added question1

---
+++ Desktop/untitled folder/untitled folder 2/ASOC Projects/Github2/HashMap Practice/index.js
@@ -0,0 +1,11 @@
+// Question 1:
+// Two Sum:
+// Given an array of integers and a target sum, find two numbers in the array that add up to the target sum.
+
+// Example:
+
+// makefile
+// Copy code
+// Input: [2, 7, 11, 15], target = 9
+// Output: [2, 7]
+
